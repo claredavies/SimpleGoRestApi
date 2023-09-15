7f46e927e503a0b1c6bf65bdb7756aa4b317771a
claredavies <clare.davies@abo.fi>
2023-09-15 15:43:13 +0100

    Added in helper test file

--- main_test.go
+++ main_test.go
@@ -2,13 +2,10 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 	"fmt"
 
-	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -71,39 +68,4 @@ func TestGetBookById_IdDoesNotExist(t *testing.T) {
 
         assert.Equal(t, ErrMsgBookNotFound, responseError["message"])
     }
-}
-
-func getMockRequestResponseContext(method, path string) (*echo.Echo, *httptest.ResponseRecorder, echo.Context){
-    e := echo.New()
-    SetupRoutes(e)
-
-    request := httptest.NewRequest(method, path, nil)
-    response := httptest.NewRecorder()
-    c := e.NewContext(request, response)
-
-    return e, response, c
-}
-
-func getMockResponseError(t *testing.T, response *httptest.ResponseRecorder) map[string]string {
-    var responseError map[string]string
-    if err := json.Unmarshal(response.Body.Bytes(), &responseError); err != nil {
-        t.Fatalf("Failed reading response: %s", err)
-    }
-    return responseError
-}
-
-func getMockResponseBook(t *testing.T, response *httptest.ResponseRecorder) Book {
-    var responseBook Book
-    if err := json.Unmarshal(response.Body.Bytes(), &responseBook); err != nil {
-        t.Fatalf("Failed reading response: %s", err)
-    }
-    return responseBook
-}
-
-func getMockResponseBooks(t *testing.T, response *httptest.ResponseRecorder) []Book {
-     var responseBooks []Book
-     if err := json.Unmarshal(response.Body.Bytes(), &responseBooks); err != nil {
-        t.Fatalf("Failed reading response: %s", err)
-     }
-     return responseBooks
 }
