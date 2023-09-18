e509483018eca52daac7a4033962c7ec01a006b5
claredavies <clare.davies@abo.fi>
2023-09-18 11:22:42 +0100

    Added in constants for error

---
+++ constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+    ErrMsgParamIDRequired    = "ID parameter is required."
+    ErrMsgBookNotFound  = "Book not found."
+    ErrMsgQueryIDRequired    = "Query ID parameter is required."
+    ErrMsgNoBooksRemaining = "All books out on loan"
+    ErrInvalidJSON = "Invalid JSON"
+)
